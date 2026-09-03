649142ebfc4a6a8aab8e4f3953eb73788a2345b8
Maciej <maciej.bartosiak10@gmail.com>
2026-09-03 22:03:12 +0200

    Dodano endpoint POST do odbierania formularza kontaktowego

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
+app.use(cors());
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Serwer KS Lechia Grodzisk działa!');
+});
+
+app.post('/api/kontakt', (req, res) => {
+    const { imie, email, temat, wiadomosc } = req.body;
+
+    console.log('Nowa wiadomość z formularza:');
+    console.log({ imie, email, temat, wiadomosc });
+
+    res.json({ sukces: true, komunikat: 'Wiadomość odebrana przez serwer!' });
 });
 
 app.listen(PORT, () => {
